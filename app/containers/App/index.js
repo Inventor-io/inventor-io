7fb6fa4ebb60e93c990ef6d74b1f0a215423724c
johnpwarren@gmail.com <johnpwarren@gmail.com>
2018-11-09 11:51:15 -0600

    added restaurant route

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -14,6 +14,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Restaurant from 'containers/Restaurant/Loadable';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
@@ -41,6 +42,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/features" component={FeaturePage} />
+        <Route path="/restaurant" component={Restaurant} />
         <Route path="" component={NotFoundPage} />
       </Switch>
       <Footer />
