51592e7bfe2dce723f0177068bddfdd0baf3dba2
Nikolas Mentakis <nmentakis@gmail.com>
2018-11-13 08:26:58 -0600

    begin setup of constants

--- app/containers/LoginPage/constants.js
+++ app/containers/LoginPage/constants.js
@@ -5,3 +5,5 @@
  */
 
 export const DEFAULT_ACTION = 'app/LoginPage/DEFAULT_ACTION';
+export const UPDATE_USERNAME = 'app/LoginPage/UPDATE_USERNAME';
+export const UPDATE_PASSWORD = 'app/LoginPage/UPDATE_PASSWORD';
