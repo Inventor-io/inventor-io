f5315c96a7246a7d8626dbefc37c8e590323e62f
John Warren <johnpwarren@gmail.com>
2018-11-26 10:49:48 -0600

    button added

--- app/components/RestaurantCard/index.js
+++ app/components/RestaurantCard/index.js
@@ -10,18 +10,20 @@ import PropTypes from 'prop-types';
 import { Card, Icon } from 'semantic-ui-react';
 
 const RestaurantCard = props => (
-  <Card>
-    <Card.Content header={props.header} />
-    <Card.Content description={props.description} />
-    <Card.Content extra>
-      <Link to="/dashboard">
-        <Icon onClick={props.click} name="info" id={props.id} />
-      </Link>
-    </Card.Content>
-    {/* <Card.Content extra>
+  <Card.Group itemsPerRow={4}>
+    <Card>
+      <Card.Content header={props.header} />
+      <Card.Content description={props.description} />
+      <Card.Content extra>
+        <Link to="/dashboard">
+          <Icon onClick={props.click} name="info" id={props.id} />
+        </Link>
+      </Card.Content>
+      {/* <Card.Content extra>
       <Icon name= />4 Friends {JSON.stringify(props)}
     </Card.Content> */}
-  </Card>
+    </Card>
+  </Card.Group>
 );
 
 RestaurantCard.propTypes = {
--- app/containers/RestaurantList/index.js
+++ app/containers/RestaurantList/index.js
@@ -10,7 +10,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 import { Redirect } from 'react-router-dom';
-// import { Button } from 'semantic-ui-react';
+import { Button, Card } from 'semantic-ui-react';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
@@ -20,7 +20,7 @@ import saga from './saga';
 import { getRestaurants } from './actions';
 import RestaurantCard from '../../components/RestaurantCard';
 import { selectedRes } from '../RestaurantDashboard/actions';
-
+import history from '../../utils/history';
 /* eslint-disable react/prefer-stateless-function */
 export class RestaurantList extends React.Component {
   // componentDidMount() {
@@ -34,23 +34,31 @@ export class RestaurantList extends React.Component {
   render() {
     return (
       <div>
-        {this.props.restaurantList.restaurants
-          ? this.props.restaurantList.restaurants.map((restaurant, key) => (
-              <RestaurantCard
-                key={key}
-                header={restaurant.restaurants_name}
-                click={this.props.onClick}
-                id={restaurant.id}
-                description={
-                  <ul>
-                    <li>{restaurant.restaurant_address}</li>
-                    <li>{restaurant.restaurant_phone_number}</li>
-                    <li>{restaurant.restaurant_website}</li>
-                  </ul>
-                }
-              />
+        <div>
+          Welcome {sessionStorage.getItem('username')}
+          <Button floated="right" onClick={this.props.addRestaurant}>
+            Add Restaurant
+          </Button>
+        </div>
+        {this.props.restaurantList.restaurants ? (
+
+            this.props.restaurantList.restaurants.map((restaurant, key) => (
+            <RestaurantCard
+              key={key}
+              header={restaurant.restaurants_name}
+              click={this.props.onClick}
+              id={restaurant.id}
+              description={
+                <ul>
+                  <li>{restaurant.restaurant_address}</li>
+                  <li>{restaurant.restaurant_phone_number}</li>
+                  <li>{restaurant.restaurant_website}</li>
+                </ul>
+              }
+            />
             ))
-          : null}
+
+        ) : null}
         {/* <Button content="get Repos" onClick={this.props.onPageLoad} /> */}
       </div>
     );
@@ -77,6 +85,9 @@ function mapDispatchToProps(dispatch) {
       const resID = e.target.id;
       dispatch(selectedRes(resID));
       console.log(resID);
+    },
+    addRestaurant: () => {
+      history.push('/addrestaurant');
     },
   };
 }
