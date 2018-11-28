49908b5c562542991bf1ece1aeb7b5cadf11e42a
Yirey Suh <yrysuh@gmail.com>
2018-11-28 17:34:07 -0600

    add shopping cart container

---
+++ app/containers/ShoppingCart/actions.js
@@ -0,0 +1,52 @@
+/*
+ *
+ * ShoppingCart actions
+ *
+ */
+
+import {
+  DELETE_ITEM,
+  PLACE_ORDER,
+  CHANGE_INPUT,
+  CALL_MOUNT_ORDER,
+  SEND_MOUNT_ORDER,
+  WIPE_CART,
+} from './constants';
+
+export function deleteItem(i) {
+  return {
+    type: DELETE_ITEM,
+    del: i,
+  };
+}
+
+export function placeOrder() {
+  return {
+    type: PLACE_ORDER,
+  };
+}
+
+export function handleInput() {
+  return {
+    type: CHANGE_INPUT,
+  };
+}
+
+export function mountOrder() {
+  return {
+    type: CALL_MOUNT_ORDER,
+  };
+}
+
+export function sendMountOrder(arr) {
+  return {
+    type: SEND_MOUNT_ORDER,
+    orderList: arr,
+  };
+}
+
+export function wipeCart() {
+  return {
+    type: WIPE_CART,
+  };
+}
---
+++ app/containers/ShoppingCart/constants.js
@@ -0,0 +1,12 @@
+/*
+ *
+ * ShoppingCart constants
+ *
+ */
+
+export const DELETE_ITEM = 'app/ShoppingCart/DELETE_ITEM';
+export const PLACE_ORDER = 'app/ShoppingCart/PLACE_ORDER';
+export const CHANGE_INPUT = 'app/ShoppingCart/CHANGE_INPUT';
+export const CALL_MOUNT_ORDER = 'app/ShoppingCart/CALL_MOUNT_ORDER';
+export const SEND_MOUNT_ORDER = 'app/ShoppingCart/SEND_MOUNT_ORDER';
+export const WIPE_CART = 'app/ShoppingCart/WIPE_CART';
