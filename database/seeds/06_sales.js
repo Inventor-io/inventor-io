77705d2d3c41fa46d323dd7f8d597afd6d94d373
Yirey Suh <yrysuh@gmail.com>
2018-12-01 11:07:24 -0600

    table 5 -7

--- database/seeds/06_sales.js
+++ database/seeds/06_sales.js
@@ -1,33 +1,37 @@
+const readJSONFile = require('../readJSONFile');
+const data = readJSONFile('./database/seedJSON/df_06.json');
+
 exports.seed = knex =>
   knex('sales')
     .del()
     .then(() =>
       // Inserts seed entries
-      knex('sales').insert([
-        // {
-        //   recipe_id: 1,
-        //   quantity: 3,
-        //   restaurant_id: 1,
-        // },
-        // {
-        //   recipe_id: 2,
-        //   quantity: 20,
-        //   restaurant_id: 1,
-        // },
-        // {
-        //   recipe_id: 1,
-        //   quantity: 5,
-        //   restaurant_id: 1,
-        // },
-        // {
-        //   recipe_id: 2,
-        //   quantity: 15,
-        //   restaurant_id: 1,
-        // },
-        // {
-        //   recipe_id: 3,
-        //   quantity: 2,
-        //   restaurant_id: 1,
-        // },
-      ]),
+      // knex('sales').insert([
+      //   {
+      //     recipe_id: 1,
+      //     quantity: 3,
+      //     restaurant_id: 1,
+      //   },
+      //   {
+      //     recipe_id: 2,
+      //     quantity: 20,
+      //     restaurant_id: 1,
+      //   },
+      //   {
+      //     recipe_id: 1,
+      //     quantity: 5,
+      //     restaurant_id: 1,
+      //   },
+      //   {
+      //     recipe_id: 2,
+      //     quantity: 15,
+      //     restaurant_id: 1,
+      //   },
+      //   {
+      //     recipe_id: 3,
+      //     quantity: 2,
+      //     restaurant_id: 1,
+      //   },
+      // ]),
+      knex('sales').insert(data),
     );
