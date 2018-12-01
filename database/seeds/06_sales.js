2bd97d79da8f062bfc86be769f4324cb6b2eb957
Yirey Suh <yrysuh@gmail.com>
2018-11-30 19:59:55 -0600

    table 5-7

--- database/seeds/06_sales.js
+++ database/seeds/06_sales.js
@@ -4,30 +4,30 @@ exports.seed = knex =>
     .then(() =>
       // Inserts seed entries
       knex('sales').insert([
-        {
-          recipe_id: 1,
-          quantity: 3,
-          restaurant_id: 1,
-        },
-        {
-          recipe_id: 2,
-          quantity: 20,
-          restaurant_id: 1,
-        },
-        {
-          recipe_id: 1,
-          quantity: 5,
-          restaurant_id: 1,
-        },
-        {
-          recipe_id: 2,
-          quantity: 15,
-          restaurant_id: 1,
-        },
-        {
-          recipe_id: 3,
-          quantity: 2,
-          restaurant_id: 1,
-        },
+        // {
+        //   recipe_id: 1,
+        //   quantity: 3,
+        //   restaurant_id: 1,
+        // },
+        // {
+        //   recipe_id: 2,
+        //   quantity: 20,
+        //   restaurant_id: 1,
+        // },
+        // {
+        //   recipe_id: 1,
+        //   quantity: 5,
+        //   restaurant_id: 1,
+        // },
+        // {
+        //   recipe_id: 2,
+        //   quantity: 15,
+        //   restaurant_id: 1,
+        // },
+        // {
+        //   recipe_id: 3,
+        //   quantity: 2,
+        //   restaurant_id: 1,
+        // },
       ]),
     );
